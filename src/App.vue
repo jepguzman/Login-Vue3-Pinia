72799a3d86a305820b11dae8b28665b2bc5973cb
9jack9 <jackson55adl@gmail.com>
2023-04-06 10:57:05 +0200

    Proyect started, added main pages

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+import Login from '@/views/Login.vue'
+</script>
+
+<template>
+  <div class="main-wall">
+    <div class="main-wall__more">
+      <h1>Prepare-ya!</h1>
+      <p>Where correctness comes to reality</p>
+      <img src="@/assets/imgs/organica.png" alt="" />
+    </div>
+    <div class="main-wall__content">
+      <Login />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.main-wall {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  gap: 10px;
+
+  &__more {
+    height: 600px;
+    background-color: lightgreen;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    border-radius: 5px;
+    padding-top: 50px;
+
+    img {
+      height: 500px;
+    }
+  }
+
+  &__content {
+  }
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,63 @@
+<script setup></script>
+
+<template>
+  <div class="login-card">
+    <h2 class="login-card__title">Connect to your account</h2>
+    <input class="login-card__input" type="text" placeholder="Email" />
+    <input class="login-card__input" type="text" placeholder="Password" />
+    <p>Forgot password?</p>
+    <button>Get inside!</button>
+    <span
+      >Haven't you join us!
+      <p>Subscribe now!</p></span
+    >
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.login-card {
+  width: 400px;
+  height: 600px;
+  background-color: lightcoral;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  border-radius: 4px;
+
+  gap: 20px;
+
+  &__title {
+  }
+
+  &__input {
+    height: 30px;
+    width: 300px;
+    border-style: none;
+    padding: 5px;
+    border-radius: 4px;
+  }
+
+  button {
+    height: 30px;
+    width: 300px;
+    border-radius: 4px;
+    background-color: white;
+    border-style: none;
+  }
+
+  span {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    gap: 5px;
+
+    p {
+      color: blue;
+    }
+  }
+}
+</style>
