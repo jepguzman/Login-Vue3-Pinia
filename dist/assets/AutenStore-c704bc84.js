5264c76c39a08a9b8d1d7abcee3409d7671d5c8f
jepguzman <jepg.movil@gmail.com>
2024-07-22 19:09:03 -0600

    ok

---
+++ dist/assets/AutenStore-c704bc84.js
@@ -0,0 +1,1 @@
+import{m as e}from"./index-e6e798a2.js";const o=e({id:"autenStore",state:()=>({idalumno:{},nombre:{}}),getters:{},actions:{}});export{o as u};
---
+++ dist/assets/pwd-d1d7341f.js
@@ -0,0 +1,1 @@
+const s="/assets/user-022373d0.svg",t="/assets/pwd-508c7b20.svg";export{s as _,t as a};
